088c929e099d117561e0b200055f99c5b9f4fda1
LJS360d <luca.lencioni09@gmail.com>
2025-12-24 18:34:05 +0100

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
+const [username, repoName] = GITHUB_REPOSITORY
+  ? GITHUB_REPOSITORY.split("/")
+  : ["", ""];
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: GITHUB_REPOSITORY
+    ? `https://${username}.github.io`
+    : "http://localhost:4321",
+  // If in GitHub Actions, use /repo-name/ as the subpath
+  base: GITHUB_REPOSITORY ? `/${repoName}/` : "/",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
